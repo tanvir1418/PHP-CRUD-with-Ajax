13d7882d2e9da856eaa4f80e7d41409dfcce5663
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2020-12-31 19:37:18 +0600

    Ajax - Insert Data

---
+++ ajax-insert.php
@@ -0,0 +1,14 @@
+<?php
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+
+    $conn = mysqli_connect("localhost","root","","test")  or die("Connection failed");
+
+    $sql = "INSERT INTO students(first_name, last_name) VALUES('{$first_name}','{$last_name}')";
+
+    if(mysqli_query($conn, $sql)){
+        echo 1;
+    }else{
+        echo 0;
+    }
+?>
--- show-data.php
+++ show-data.php
@@ -14,17 +14,37 @@
         #main{
             margin: 10px auto;
         }
-        #load-button{
+        #save-button{
             color: black;
             background-color: wheat;
             padding: 10px;
             margin: 20px;
             border-radius: 15px;
         }
-        #load-button:hover{
+        #save-button:hover{
             color: wheat;
             background-color: black;
             cursor: pointer;
+        }
+        #success-message{
+            background: #DEF1D8;
+            color: green;
+            padding: 10px;
+            margin: 10px;
+            display: none;
+            position: absolute;
+            right: 15px;
+            top: 15px;
+        }
+        #error-message{
+            background: #EFDCDD;
+            color: red;
+            padding: 10px;
+            margin: 10px;
+            display: none;
+            position: absolute;
+            right: 15px;
+            top: 15px;
         }
     </style>
     <title>Ajax Basic</title>
@@ -37,26 +57,35 @@
             </td>
         </tr>
         <tr>
-            <td id="table-load">
-                <input type="button" id="load-button" value="Load Data">
+            <td id="table-form">
+                <!-- form tag is only use for resetting those input field using jQuery -->
+                <form id="addForm">
+                    First Name : <input type="text" id="fname">&nbsp;&nbsp;
+                    Last Name : <input type="text" id="lname">
+                    <input type="submit" id="save-button" value="Save">
+                </form>
             </td>
         </tr>
         <tr>
             <td id="table-data">
                 <table border="1" width="100%" cellspacing="0" cellpadding="10px">
-                
+
                 </table>
             </td>
         </tr>
     </table>
+
+    <div id="error-message"></div>
+    <div id="success-message"></div>
+
+
     <script type="text/javascript" src="js/jquery.js"></script>
-     <script type="text/javascript">
+    <script type="text/javascript">
      
         // When the document is ready then javascript will start working
-        $(document).ready(function(){               
-            
-            // We want to implement ajax functionality on button whose id is 'load button' and the ajax basic syntax.
-            $("#load-button").on("click", function(e){
+        $(document).ready(function(){   
+
+            function loadTable(){
                 $.ajax({
                     // Where the ajax query will perform
                     url: "ajax-load.php",
@@ -68,7 +97,41 @@
                         $("#table-data").html(data);
                     }
                 });
+            }
+
+            loadTable();
+
+            $("#save-button").on("click", function(e){
+                // prevent default is used to stop the default activity
+                e.preventDefault();
+
+                var fname = $("#fname").val();
+                var lname = $("#lname").val();
+
+                if(fname == "" || lname == ""){
+                    $("#error-message").html("All fields are required.").slideDown();
+                    $("#success-message").slideUp();
+                }else{
+                    $.ajax({
+                        url : "ajax-insert.php",
+                        type : "POST",
+                        data : {first_name: fname, last_name: lname}, //data sent in object form
+                        success : function(data){
+                            if(data == 1){
+                                loadTable();  //Table is loaded after insertion
+                                $("#addForm").trigger("reset");
+                                $("#success-message").html("Data Inserted Successfully.").slideDown();
+                                $("#error-message").slideUp();
+                            }else{
+                                $("#error-message").html("Can't Save Record.").slideDown();
+                                $("#success-message").slideUp();
+                            }                         
+                        }
+                    });
+                }
+                
             });
+
         });
     </script>
 </body>
